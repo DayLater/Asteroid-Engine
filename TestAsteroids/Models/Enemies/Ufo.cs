fcbd629df9d526b7d146a759eb9a115524ecacdb
Besh <besh98@bk.ru>
2020-09-13 16:49:45 +0400

    IBehavior и ее реализации перенесены в отдельную папку. Изменен класс Ufo. 1. Удалено переопределение метод Update 2. Body и Head теперь возвращают при каждом обращении новый эллипс

--- TestAsteroids/Models/Enemies/Ufo.cs
+++ TestAsteroids/Models/Enemies/Ufo.cs
@@ -5,23 +5,15 @@ namespace AsteroidsEngine.Entities
 {
     public class Ufo: Enemy
     {
-        public Ellipse Body { get;  }
-        public Ellipse Head { get;  }
+        public Ellipse Body => new Ellipse(30, 15, Position);
+        public Ellipse Head => new Ellipse(15, 15, Position + new Vector(0, -Body.Ry * 0.7f));
+
         public override int Value { get; } = 100;
 
         public Ufo(Vector position, IBehavior behavior) : base(behavior)
         {
             Position = position;
-            Body = new Ellipse(30, 15, Position);
-            Head = new Ellipse(15, 15, Position + new Vector(0, -Body.Ry * 0.7f));
             Speed = GetRandomNotZeroVector(-2, 2);
-        }
-
-        public override void Update()
-        { 
-            base.Update();
-            Body.Center = Position;
-            Head.Center = Position + new Vector(0, -Body.Ry*0.7f);
         }
 
         public override bool Contains(Vector vector)
